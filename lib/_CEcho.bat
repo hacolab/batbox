e3f653d15e69ad6a539dc0fae6e607a7a653e3ed
coro <hacorolab@gmail.com>
2018-12-15 21:50:43 +0900

    CEchoの色付き指定文字列省略時は全文字列を色付きで表示するように変更

--- lib/_CEcho.bat
+++ lib/_CEcho.bat
@@ -2,16 +2,29 @@
 setlocal enabledelayedexpansion
 REM ----------------------------------------------------------------------
 REM - NAME   ) _CEcho
-REM - DESC   ) colorful string echo
-REM - USAGE  ) call _CEcho string withColorStr colorIndex
+REM - DESC   ) Echo colorful text
+REM - USAGE  ) Call _CEcho string [subStr] colorIndex
 REM - IN/OUT )
-REM -  IN   %1  : echo all string
-REM -  IN   %2  : color string
-REM -  IN   %2* : color index (_Cx_COLORNAME :see _InitColors.bat)
+REM -  IN   %1  : Echo string
+REM -  IN  [%2] : substring with coloring echo (default: all %1 string)
+REM -  IN   %3  : Color index (_Cx_COLORNAME :see _InitColors.bat)
+REM - TODO!  ) Support case-insensitive
 REM ----------------------------------------------------------------------
 :_CEcho
 set echoText=%~1
-set colorCode=!_COLOR_CODE[%3]!
-set echoText=!echoText:%~2=%colorCode%%~2%_COLOR_RESET%!
+if not "%~3"=="" (
+    set colorCode=!_COLOR_CODE[%~3]!
+    call :Replace echoText "%~2" "!colorCode!%~2%_COLOR_RESET%"
+) else (
+    set colorCode=!_COLOR_CODE[%~2]!
+    set echoText=!colorCode!!echoText!!_COLOR_RESET!
+)
 echo %echoText%
+exit /b
+
+:Replace
+set allStr=!%~1!
+set beforeStr=%~2
+set afterStr=%~3
+set %~1=!allStr:%~2=%~3!
 exit /b
