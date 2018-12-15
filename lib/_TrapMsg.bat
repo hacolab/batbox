9b7194487d34182d6eec4a9e982b1588d225402b
coro <hacorolab@gmail.com>
2018-12-16 02:46:16 +0900

    TrpMsg：メッセージはそのまま表示

--- lib/_TrapMsg.bat
+++ lib/_TrapMsg.bat
@@ -2,13 +2,13 @@
 setlocal enabledelayedexpansion
 REM ----------------------------------------------------------------------
 REM - NAME   ) _TrapMsg
-REM - DESC   ) Print message "Name:Value" when only debug mode(_DEBUG_MODE_=1)
+REM - DESC   ) Print message when only debug mode(_DEBUG_MODE_=1)
 REM - USAGE  ) call _TrapMsg message
 REM - IN/OUT )
-REM -  IN  [%1] : Trap message
+REM -  IN   %1  : Trap message
 REM ----------------------------------------------------------------------
 :_TrapMsg
 if "%_DEBUG_MODE_%"=="%_TRUE_%" (
-    echo %1:!%1! >&2
+    echo %~1 >&2
 )
 exit /b
